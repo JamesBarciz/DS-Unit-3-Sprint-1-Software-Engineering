f99fbad16689754c62532e4e6a204c64c71bb225
James Barciz <jamesjbarciz@gmail.com>
2020-01-22 22:01:37 -0500

    show new changes

---
+++ module4-software-testing-documentation-and-licensing/quality_commenting.py
@@ -0,0 +1,17 @@
+### COMMENT TYPES
+
+# Inline comment
+#   - Brief annotations to indicate the meaning of a line
+#     or a few lines of code
+
+"""
+Multi
+Line
+Comments
+------------
+AKA 'docstrings' - strings ignored by the interpreter - are
+are documentation and can describe details such as the argument
+and return types of a function, links to resources, etc.  It is
+what the help() function grabs to explain things in the repl, and
+there's a tool named Pydoc that extracts docstrings and generates HTML.
+"""
